8e8513efc199e25aa9b705e367700909bec62617
David Adarkwah <72516754+Adark-Amal@users.noreply.github.com>
2020-12-21 17:04:34 +0000

    Created game window and background settings

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -5,5 +5,5 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 900
-        self.screen_height = 450
+        self.screen_height = 500
         self.bg_color = (230, 230, 230)
